4d947757104a29efe779e3f56d4fbde3239fc29e
MayankAri <atrimayank110@gmail.com>
2023-04-09 21:05:56 +0530

    Frontend half done

--- app/src/main/java/com/example/sleeptrackerdemo/MainActivity.java
+++ app/src/main/java/com/example/sleeptrackerdemo/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.sleeptrackerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button previous_Sleep,trouble;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        previous_Sleep = findViewById(R.id.previous);
+        trouble = findViewById(R.id.trouble);
+
+        //Onclick Listener
+        previous_Sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent history_activity = new Intent(MainActivity.this,PreviousSleep.class);
+                startActivity(history_activity);
+            }
+        });
+        trouble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent trouble_Sleeping = new Intent(MainActivity.this,TroubleSleeping.class);
+                startActivity(trouble_Sleeping);
+            }
+        });
         
     }
 }
---
+++ app/src/main/java/com/example/sleeptrackerdemo/TroubleSleeping.java
@@ -0,0 +1,14 @@
+package com.example.sleeptrackerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TroubleSleeping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trouble_sleeping);
+    }
+}
